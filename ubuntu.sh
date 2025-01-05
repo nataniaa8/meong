988f8a57fa15087b0f1099731e01a05596a70a21
nataniaa8 <natania.niaa08@gmail.com>
2025-01-05 12:26:22 +0700

    ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,49 +1,61 @@
 #!/bin/bash
 
-#               .-') _   .-') _       ('-.          .-') _               ('-.     
-#              ( OO ) ) (  OO) )     ( OO ).-.     ( OO ) )             ( OO ).-. 
-#  ,-.-')  ,--./ ,--,'  /     '._    / . --. / ,--./ ,--,'    ,-.-')    / . --. / 
-#  |  |OO) |   \ |  |\  |'--...__)   | \-.  \  |   \ |  |\    |  |OO)   | \-.  \  
-#  |  |  \ |    \|  | ) '--.  .--' .-'-'  |  | |    \|  | )   |  |  \ .-'-'  |  | 
-#  |  |(_/ |  .     |/     |  |     \| |_.'  | |  .     |/    |  |(_/  \| |_.'  | 
-# ,|  |_.' |  |\    |      |  |      |  .-.  | |  |\    |    ,|  |_.'   |  .-.  | 
-#(_|  |    |  | \   |      |  |      |  | |  | |  | \   |   (_|  |      |  | |  | 
-#  `--'    `--'  `--'      `--'      `--' `--' `--'  `--'     `--'      `-' `--'
+               .-') _   .-') _       ('-.          .-') _               ('-.     
+              ( OO ) ) (  OO) )     ( OO ).-.     ( OO ) )             ( OO ).-. 
+  ,-.-')  ,--./ ,--,'  /     '._    / . --. / ,--./ ,--,'    ,-.-')    / . --. / 
+  |  |OO) |   \ |  |\  |'--...__)   | \-.  \  |   \ |  |\    |  |OO)   | \-.  \  
+  |  |  \ |    \|  | ) '--.  .--' .-'-'  |  | |    \|  | )   |  |  \ .-'-'  |  | 
+  |  |(_/ |  .     |/     |  |     \| |_.'  | |  .     |/    |  |(_/  \| |_.'  | 
+ ,|  |_.' |  |\    |      |  |      |  .-.  | |  |\    |    ,|  |_.'   |  .-.  | 
+(_|  |    |  | \   |      |  |      |  | |  | |  | \   |   (_|  |      |  | |  | 
+  `--'    `--'  `--'      `--'      `--' `--' `--'  `--'     `--'      `-' `--'
 
-# Otomasi by Intania ⋆.˚🦋༘⋆
+echo "=============================================="
+echo "   🦋 Welcome to Natania's Automation 🦋   "
+echo "=============================================="
 
-echo "Configuring VLAN and DHCP on Ubuntu Server with Kartolo Datautama repository..."
+echo "Configuring Ubuntu Server"
 
-# Menambahkan repository Kartolo Datautama
-echo "Adding Kartolo Datautama repository..."
-echo "deb [trusted=yes] http://kartolo.sby.datautama.net.id/ubuntu focal main universe restricted multiverse" | tee /etc/apt/sources.list.d/kartolo-datautama.list
+# Manambahkan Repository
+cat <<EOF > /etc/apt/sources.list
+deb http://kartolo.sby.datautama.net.id/ubuntu/ focal main restricted universe multiverse
+deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-updates main restricted universe multiverse
+deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-security main restricted universe multiverse
+deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-backports main restricted universe multiverse
+deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-proposed main restricted universe multiverse
 
-# Update package index
-apt update
+# Pasang DHCP server
+apt update && apt install -y isc-dhcp-server
 
-# Assign IP address to VLAN interface
-ip link add link eth1 name eth1.10 type vlan id 10
-ip addr add 192.168.16.1/24 dev eth1.10
-ip link set up eth1.10
+# Konfigurasi VLAN dan IP
+cat <<EOF > /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      dhcp4: true
+    eth1:
+      addresses: []
+  vlans:
+    eth1.10:
+      id: 10
+      link: eth1
+      addresses:
+        - 192.168.16.1/24
+EOF
 
-# Install and configure DHCP server
-apt install -y isc-dhcp-server
+netplan apply
 
-# Configure DHCP for VLAN 10
-cat <<EOL > /etc/dhcp/dhcpd.conf
-# Watermark: niaaa
+# Konfigurasi DHCP server
+cat <<EOF > /etc/dhcp/dhcpd.conf
 subnet 192.168.16.0 netmask 255.255.255.0 {
   range 192.168.16.10 192.168.16.100;
   option routers 192.168.16.1;
   option domain-name-servers 8.8.8.8, 8.8.4.4;
 }
-EOL
+EOF
 
-# Set the DHCP server to listen on eth1.10
-echo "INTERFACESv4=\"eth1.10\"" > /etc/default/isc-dhcp-server
-
-# Restart DHCP service
 systemctl restart isc-dhcp-server
-systemctl enable isc-dhcp-server
-
-echo "Ubuntu Server configuration completed!"
+echo "[Natania] VLAN and network configuration applied."
+echo "🎉 Otomasi selesai oleh Intania bersama meong!"
